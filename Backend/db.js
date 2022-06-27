653d5f9011e8d952573cddc99af156af05049a82
Angad-Godara <angadrrr2002@gmail.com>
2022-06-27 18:09:19 +0530

    ADD: Ready to host

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose')
+const passcode = require('./local')
 
-const mongoURI = process.env.URI;
+const mongoURI = `mongodb+srv://lostinpresent:${passcode}@cluster0.a5ibf.mongodb.net/test`;
 
 const connectToMongo = () => {
     mongoose.connect(mongoURI, () => {
-        console.log("Connection sucessfulL");
+        console.log("Connection sucessfull");
     }) /// THIS IS ASYNC THAT'S WHY IT TAKES SOME EXTRA TIME TO GET CONNECTED
 }
 
